880493abe4eaf227b3b3e30c44b4754231f70d3c
Aditya Bikram Biswas <49611171+AdityaBiswas1994@users.noreply.github.com>
2025-07-11 12:07:30 +0200

    Logo on FedISIC added

--- LoGo-main/query_strategies/fal/__init__.py
+++
@@ -1,4 +0,0 @@
-from .ensemble_logit import EnsLogitEntropy, EnsLogitBadge
-from .ensemble_rank import EnsRankEntropy, EnsRankBadge
-from .finetuning import FTEntropy, FTBadge
-from .logo import LoGo
--- LoGo-main/util/__init__.py
+++
@@ -1,5 +0,0 @@
-from .args import *
-from .path import *
-from .data_simulator import *
-from .longtail_dataset import *
-from .misc import *
